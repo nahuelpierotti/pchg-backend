c088d43b6f37c2d5685885f0ce61760f74feb114
Nahuel Pierotti <nahuel.pierotti@compliancemetrix.com>
2025-02-17 10:12:03 -0300

    Fixes lombok issue

--- src/main/java/com/pchg/model/Player.java
+++ src/main/java/com/pchg/model/Player.java
@@ -1,5 +1,7 @@
 package com.pchg.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,10 +9,12 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Player {
+    @Id
     private Integer id;
     private Integer userId;
-    private Position position;
+    //private Position position;
     private Integer condition;
 
 }
